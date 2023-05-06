58fcf9c40d3ccec7e3a97e8ba3843c4a7771f2e6
James Fitzgerald <james.fitzgerald2011@hotmail.co.uk>
2023-05-06 10:12:58 +0100

    Remove unused constructor

--- CrosswordGenerator/Generator/Models/BlockCoordinates.cs
+++ CrosswordGenerator/Generator/Models/BlockCoordinates.cs
@@ -2,29 +2,22 @@
 {
     public class BlockCoordinates
     {
-        public int Y { get; set; }
-        public int X { get; set; }
-
-
-        public int ArrayY => Y - 1;
-        public int ArrayX => X - 1;
-
         public BlockCoordinates(int x, int y)
         {
             X = x;
             Y = y;
         }
 
+        public int Y { get; set; }
+        public int X { get; set; }
+
+        public int ArrayY => Y - 1;
+        public int ArrayX => X - 1;
+
         public void ShiftCoordinates(int x, int y)
         {
             X += x;
             Y += y;
-        }
-
-        public BlockCoordinates()
-        {
-            Y = 0;
-            X = 0;
         }
     }
 }

90a68653b91682ea38413f321632a1ff2847055d
James Fitzgerald <james.fitzgerald2011@hotmail.co.uk>
2023-05-19 21:54:09 +0100

    Add test to test efficiency

--- CrosswordGeneratorTests/GenerationManagerTests.cs
+++ CrosswordGeneratorTests/GenerationManagerTests.cs
@@ -15,6 +15,11 @@ namespace CrosswordGeneratorTests
 
         private Mock<IGenerator> _generatorMock;
 
+        private const int Best5Size = 56;
+        private const int Best10Size = 120;
+        private const int Best15Size = 168;
+        private const int Best15UnSize = 143;
+
         private readonly List<WordCluePair> _twoUnplacedWords = new()
         {
             new WordCluePair("Goat"),
@@ -346,11 +351,22 @@ namespace CrosswordGeneratorTests
         {
             var watch = new Stopwatch();
 
+            var lowest5Size = int.MaxValue;
+            var lowest10Size = int.MaxValue;
+            var lowest15Size = int.MaxValue;
+            var lowest15UnSize = int.MaxValue;
+
+            double averageSizeScore = 0;
+
             // Arrange
+            const int maxAttempts = 100;
+            const int maxGenerations = 50;
+            const int timesToRepeat = 10;
+
             double averageSuccess = 0;
             double averageTime = 0;
-            const int timesToRepeat = 100000;
             var wordSelector = 1;
+
             // Act
             for (var i = 0; i < timesToRepeat; i++)
             {
@@ -364,7 +380,7 @@ namespace CrosswordGeneratorTests
 
                 watch.Start();
 
-                var result = _realManager.GenerateCrosswords(words, timeout: int.MaxValue, cullIdenticals: false).ToList();
+                var result = _realManager.GenerateCrosswords(words, maxAttempts: maxAttempts, maxGenerations: maxGenerations, int.MaxValue, cullIdenticals: false).ToList();
 
                 watch.Stop();
                 averageTime += watch.ElapsedMilliseconds;
@@ -375,18 +391,129 @@ namespace CrosswordGeneratorTests
                     ? Convert.ToInt32(result.Any(x => x.NumberOfUnplacedWords == 3))
                     : Convert.ToInt32(result.Any(x => x.NumberOfUnplacedWords == 0));
                 averageSuccess += success;
+
+
+                if (result.Any())
+                {
+                    var blockSize = result.Min(r => r.BlocksSize);
+                    switch (wordSelector)
+                    {
+                        case 1:
+                            averageSizeScore += (double)Best5Size / blockSize;
+                            lowest5Size = blockSize < lowest5Size ? blockSize : lowest5Size;
+                            break;
+                        case 2:
+                            averageSizeScore += (double)Best10Size / blockSize;
+                            lowest10Size = blockSize < lowest10Size ? blockSize : lowest10Size;
+                            break;
+                        case 3:
+                            averageSizeScore += (double)Best15Size / blockSize;
+                            lowest15Size = blockSize < lowest15Size ? blockSize : lowest15Size;
+                            break;
+                        case 4:
+                            averageSizeScore += (double)Best15UnSize / blockSize;
+                            lowest15UnSize = blockSize < lowest15UnSize ? blockSize : lowest15UnSize;
+                            break;
+
+                    }
+                }
                 wordSelector = wordSelector == 4 ? 1 : wordSelector + 1;
             }
 
             averageSuccess = (averageSuccess / timesToRepeat) * 100;
+            averageSizeScore = (averageSizeScore / timesToRepeat) * 100;
             averageTime /= timesToRepeat;
 
             // Assert
             TestContext.WriteLine($"Success rate: {averageSuccess}%");
             TestContext.WriteLine($"Average time: {averageTime}ms");
+            TestContext.WriteLine();
+            TestContext.WriteLine($"Lowest size 5 words: {lowest5Size} ({Best5Size})");
+            TestContext.WriteLine($"Lowest size 10 words: {lowest10Size} ({Best10Size})");
+            TestContext.WriteLine($"Lowest size 15 words: {lowest15Size} ({Best15Size})");
+            TestContext.WriteLine($"Lowest size 15 words with 3 unplaceable: {lowest15UnSize} ({Best15UnSize})");
+            TestContext.WriteLine($"Average size score: {averageSizeScore:F}%");
+
 
             Assert.True(averageSuccess >= 99.00);
         }
+
+        [Test]
+        public void ProgressiveEfficiencyTest()
+        {
+            var watch = new Stopwatch();
+            // Arrange
+            const int timesToRepeat = 100;
+            const int maxAttempts = 35;
+
+            for (var maxGenerations = 1; maxGenerations <= 35; maxGenerations++)
+            {
+                double averageSizeScore = 0;
+                double averageSuccess = 0;
+                double averageTime = 0;
+                var wordSelector = 1;
+
+                // Act
+                for (var i = 1; i <= timesToRepeat; i++)
+                {
+                    var words = wordSelector switch
+                    {
+                        1 => _5WordsAllPlaceable,
+                        2 => _10WordsAllPlaceable,
+                        3 => _15WordsAllPlaceable,
+                        _ => _15Words3Unplaceable,
+                    };
+
+                    watch.Start();
+
+                    var result = _realManager.GenerateCrosswords(words, maxAttempts: maxAttempts,
+                        maxGenerations: maxGenerations, int.MaxValue, cullIdenticals: false).ToList();
+
+                    watch.Stop();
+                    averageTime += watch.ElapsedMilliseconds;
+                    watch.Reset();
+                    // If all words placeable, success is all words placed
+                    // If 3 words unplaceable, success is all but 3 words placed
+                    var success = wordSelector == 4
+                        ? Convert.ToInt32(result.Any(x => x.NumberOfUnplacedWords == 3))
+                        : Convert.ToInt32(result.Any(x => x.NumberOfUnplacedWords == 0));
+                    averageSuccess += success;
+
+                    if (result.Any())
+                    {
+                        var blockSize = result.Min(r => r.BlocksSize);
+                        switch (wordSelector)
+                        {
+                            case 1:
+                                averageSizeScore += (double)Best5Size / blockSize;
+                                break;
+                            case 2:
+                                averageSizeScore += (double)Best10Size / blockSize;
+                                break;
+                            case 3:
+                                averageSizeScore += (double)Best15Size / blockSize;
+                                break;
+                            case 4:
+                                averageSizeScore += (double)Best15UnSize / blockSize;
+                                break;
+                        }
+                    }
+                    wordSelector = wordSelector == 4 ? 1 : wordSelector + 1;
+                }
+
+                averageSuccess = (averageSuccess / timesToRepeat) * 100;
+                averageSizeScore = (averageSizeScore / timesToRepeat) * 100;
+                averageTime /= timesToRepeat;
+
+                // Assert
+                TestContext.WriteLine($"-- MAX Generations: {maxGenerations} --");
+                TestContext.WriteLine($"Success rate: {averageSuccess}%");
+                TestContext.WriteLine($"Average time: {averageTime}ms");
+                TestContext.WriteLine($"Average size score: {averageSizeScore:F}%");
+                TestContext.WriteLine();
+            }
+        }
+
 
         private Generation CreateDefaultGeneration()
         {
